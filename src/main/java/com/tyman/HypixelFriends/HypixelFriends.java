729b354496fe9c31a5b603a0064c377791b19a01
Tymanyay <32660892+Tymanyay@users.noreply.github.com>
2020-06-12 12:01:28 -0600

    Add files via upload

---
+++ src/main/java/com/tyman/HypixelFriends/HypixelFriends.java
@@ -0,0 +1,19 @@
+package com.tyman.HypixelFriends;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = HypixelFriends.MODID, version = HypixelFriends.VERSION)
+public class HypixelFriends
+{
+    public static final String MODID = "HypixelFriends";
+    public static final String VERSION = "1.0";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+		// some example code
+        
+    }
+}
